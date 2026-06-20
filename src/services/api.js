8be91b7fa7a4c861644b63a6d356593137c5f3e6
deep-coder1 <ndktbd@gmail.com>
2026-06-20 21:37:46 +0530

    Api url added.

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
 export const getWeather = async (city) => {
   const response = await axios.get(
-    
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'a5747b830aa5f57b581353c9d3860d07'}&units=metric`
   )
 
   return response.data
