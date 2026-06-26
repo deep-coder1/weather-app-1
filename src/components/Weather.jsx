95fcc55829daca2715cf40966fcb650188c931ae
Deepak Thakur <ndktbd@gmail.com>
2026-06-26 11:34:55 +0530

    div block add

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -31,7 +31,11 @@ function Weather() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 p-5">
-      
+      <div >
+
+        
+
+      </div>
     </div>
   );
 }
